8140d68a9b34593792df3024f774f0febd55f01b
Richard Pogoson <richardpogoson@gmail.com>
2025-08-31 11:40:36 +0100

    Readme tbd, added env argument

--- fulldockerPipeline.bat
+++ fulldockerPipeline.bat
@@ -1,8 +1,29 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Get the arg from the command line
+set "MARKET_INDEX=%1"
+
+REM Validate the arg
+if /I "%MARKET_INDEX%"=="dow" (
+    echo Using DOW index
+) else if /I "%MARKET_INDEX%"=="ftse100" (
+    echo Using FTSE 100 index
+) else if /I "%MARKET_INDEX%"=="sse50" (
+    echo Using SSE 50 index
+) else (
+    echo Invalid market index: %MARKET_INDEX%
+    echo Must be one of: dow, ftse100, sse50
+    exit /b 1
+)
+
+set "TEMP_CONFIG=temp_config.yaml"
+
+REM Use Python to inject the tag into the YAML
+python -c "import yaml; cfg = yaml.safe_load(open('configs/config.yaml')); cfg['active_index'] = r'%MARKET_INDEX%'; yaml.dump(cfg, open(r'%TEMP_CONFIG%', 'w'))"
+
 REM Check for optional flag to skip base image build
-IF "%1"=="--skip-base" (
+IF "%2"=="--skip-base" (
     echo Skipping base image build
 ) ELSE (
     echo Building base image...
@@ -17,7 +38,7 @@ docker volume create mydata
 
 echo Running hyperparameter container...
 docker run ^
-    -v "%cd%\configs:/app/data/configs" ^
+    -v "%cd%\%TEMP_CONFIG%:/app/data/configs/config.yaml" ^
     -v mydata:/app/data ^
     -p 8080:8080 ^
     --env-file .env ^
@@ -36,3 +57,5 @@ docker run ^
     --rm -it rewardtests-image
 
 echo All containers completed successfully.
+
+del "%TEMP_CONFIG%"
